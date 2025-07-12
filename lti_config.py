fd6d2e512c75467c6ae5b8e1c2c14d7f0bf65952
angeldelgadop <angeldelgadop@gmail.com>
2025-07-12 10:58:45 -0500

    Update lti_config.py

--- lti_config.py
+++ lti_config.py
@@ -1,11 +1,1 @@
-from ltiauth.fastapi import LTIAuth
-from fastapi import Depends
 
-auth = LTIAuth(
-    consumers={
-        "your-key": "your-secret"
-    }
-)
-
-def get_lti_user(user=Depends(auth)):
-    return user
